5b638d32a91f5ad861f7be60bd831f92826e230b
gerjan <gerjantd@gmail.com>
2014-02-10 19:41:17 +0100

    Improved implementation of proxy support (system environment proxy)

--- GebConfig.groovy
+++ GebConfig.groovy
@@ -11,54 +11,63 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
-/*
-Proxy proxy = new Proxy()
-proxy.setProxyAutoconfigUrl("http://wwwproxy.rivm.nl")
-DesiredCapabilities capabilities = DesiredCapabilities.htmlUnit()
-//capabilities.setCapability(CapabilityType.PROXY, proxy)
-capabilities.setCapability('PROXY', proxy)
-//WebDriver driver = new HtmlUnitDriver(capabilities)
-*/
-
 proxyHost = System.getProperty('http.proxyHost')
 proxyPort = System.getProperty('http.proxyPort')
-if (proxyHost != null && proxyPort != null) {   
-	println ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ${proxyHost}"
+
+def isProxied() {
+	return (proxyHost != null && proxyPort != null)   
+}
+
+def buildProxyCaps(caps) {
 	Proxy proxy = new Proxy()
 	proxy.setProxyType(ProxyType.MANUAL) 
 	proxy.setHttpProxy(proxyHost+":"+proxyPort)
 	proxy.setSslProxy(proxyHost+":"+proxyPort)
 	proxy.setFtpProxy(proxyHost+":"+proxyPort)
-	DesiredCapabilities capabilities = DesiredCapabilities.htmlUnit()
-	capabilities.setCapability('PROXY', proxy)
+	caps.setCapability('PROXY', proxy)
+	return caps
 }
 
-//driver = { new HtmlUnitDriver() }
-driver = { new HtmlUnitDriver(capabilities) }
-//driver = { (caps==null?new HtmlUnitDriver():new HtmlUnitDriver(caps)) }
-//driver = { new HtmlUnitDriver(caps) }
+if (isProxied) {
+	DesiredCapabilities capabilities = DesiredCapabilities.htmlUnit()
+	driver = { new HtmlUnitDriver(capabilities) }
+} else {
+	driver = { new HtmlUnitDriver() }
+}
 
 environments {
 
 	// run as “groovy -Dgeb.env=htmlunit forumslurper”
 	// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
 	htmlunit {
-		//driver = { new HtmlUnitDriver() }
-		driver = { new HtmlUnitDriver(capabilities) }
-		//driver = { (caps==null?new HtmlUnitDriver():new HtmlUnitDriver(caps)) }
-		//driver = { new HtmlUnitDriver(caps) }
+		if (isProxied) {
+			DesiredCapabilities capabilities = DesiredCapabilities.htmlUnit()
+			driver = { new HtmlUnitDriver(capabilities) }
+		} else {
+			driver = { new HtmlUnitDriver() }
+		}
 	}
 
 	// run as “groovy -Dgeb.env=chrome forumslurper”
 	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
 	chrome {
-		driver = { new ChromeDriver() }
+		if (isProxied) {
+			DesiredCapabilities capabilities = DesiredCapabilities.chrom()
+			driver = { new ChromeDriver(capabilities) }
+		} else {
+			driver = { new ChromeDriver() }
+		}
 	}
 
 	// run as “groovy -Dgeb.env=firefox forumslurper”
 	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
 	firefox {
-		driver = { new FirefoxDriver() }
+		if (isProxied) {
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox()
+			driver = { new FirefoxDriver(capabilities) }
+		} else {
+			driver = { new FirefoxDriver() }
+		}
 	}
 
 }
