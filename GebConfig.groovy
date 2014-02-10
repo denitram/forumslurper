75046449223c9c231ce86bce46ef81092d3c9070
gerjan <gerjantd@gmail.com>
2014-02-10 16:34:04 +0100

    Added GebConfig.groovy

---
+++ GebConfig.groovy
@@ -0,0 +1,32 @@
+/*
+This is the Geb configuration file.
+See: http://www.gebish.org/manual/current/configuration.html
+*/
+
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver
+
+driver = { new HtmlUnitDriver() }
+
+environments {
+
+	// run as “groovy -Dgeb.env=htmlunit forumslurper”
+	// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
+	htmlunit {
+		driver = { new HtmlUnitDriver() }
+	}
+
+	// run as “groovy -Dgeb.env=chrome forumslurper”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	chrome {
+		driver = { new ChromeDriver() }
+	}
+
+	// run as “groovy -Dgeb.env=firefox forumslurper”
+	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+	firefox {
+		driver = { new FirefoxDriver() }
+	}
+
+}
